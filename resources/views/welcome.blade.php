4af7e85935b26c50461d0756e81283974c0921ed
MirroredKube <anitha.praveenrao@gmail.com>
2024-09-01 23:10:39 +0200

    add background for home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,11 @@
 @section('title', 'Welcome to Coffee Shop')
 
 @section('content')
-    <div class="welcome">
-        <h2>Welcome to Our Coffee Shop</h2>
-        <p>Your favorite place for the best coffee in town!</p>
+<div class="background-image">
+    <div class="content">
+        <h1>Welcome to Coffee Shop</h1>
+        <p>Your favorite coffee, just a click away.</p>
+        <a href="/shop" class="btn">Shop Now</a>
     </div>
+</div>
 @endsection
