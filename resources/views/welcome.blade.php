0261e25f09e128f1c96ac12eb04043c0de2792e4
MirroredKube <anitha.praveenrao@gmail.com>
2024-09-01 17:11:35 +0200

    Next view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,7 +163,7 @@
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Coffee Company v1.2.1 2024 
                     </footer>
                 </div>
             </div>
